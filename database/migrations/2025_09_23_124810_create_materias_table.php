e21ee66eaf399c837b35e0deb7257424a06798ec
Lucas Ramalho <adryelll85@gmail.com>
2025-09-30 21:25:10 -0300

    Add: model, controller, seeder, factory para materias

--- database/migrations/2025_09_23_124810_create_materias_table.php
+++ database/migrations/2025_09_23_124810_create_materias_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('materias', function (Blueprint $table) {
             $table->id();
             $table->string('nome', 255);
+            $table->string('descricao');
             $table->unsignedBigInteger('criador');
             $table->enum('status', ["Ativo", "Inativo"])->default('Ativo');
             $table->unsignedBigInteger('ultimo_editor')->nullable();
