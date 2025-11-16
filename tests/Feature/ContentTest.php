a08ecd3ff5370dc7719b36a2d07a3507292facbf
Lucas Ramalho <75459079+Ramalho7@users.noreply.github.com>
2025-11-16 13:49:18 -0300

    Add simple tests (#72)
    * Add: laravel pint
    
    * Add: simple test
    
    * Add: run-test.yml
    
    * Add: run-test.yml
    
    * Add: run-test.yml
    
    * Add: run-test.yml
    
    * Add: run-test.yml
    
    * Add: run-test.yml
    
    * Add: run-test.yml
    
    * Add: run-test.yml
    
    * Add: run-test.yml
    
    * Add: run-test.yml
    
    * Add: run-test.yml
    
    * Apply automatic changes - pint
    
    * Add: run-test.yml
    
    * Add: run-test.yml
    
    * Apply automatic changes - pint
    
    * Add: test update content
    
    * Add: test delete content
    
    * Add: test delete content
    
    * Add: test view(index) content
    
    * Add: test view(show) content
    
    * Add: test for policies
    
    * Apply automatic changes - pint
    
    * Fix: run tests
    
    * Add: more tests para o content
    
    * Apply automatic changes - pint
    
    * Add: more tests para o content
    
    * Apply automatic changes - pint

--- tests/Feature/ContentTest.php
+++ tests/Feature/ContentTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Content;
+use App\Models\ContentTag;
 use App\Models\ContentType;
 use App\Models\SystemUser;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -620,5 +621,103 @@ class ContentTest extends TestCase
             'id' => $content->id,
             'status' => 'Ativo',
         ]);
+    }
+
+    public function test_index_supports_search_and_filters(): void
+    {
+        $user = SystemUser::factory()->create(['status' => 'Ativo', 'tipo' => 'ADM']);
+        Passport::actingAs($user);
+
+        $typeA = ContentType::factory()->create(['title' => 'Type A']);
+        $typeB = ContentType::factory()->create(['title' => 'Type B']);
+
+        $tag = \App\Models\ContentTag::factory()->create(['tag_name' => 'mytag']);
+
+        Content::factory()->create([
+            'title' => 'Searchable Title',
+            'content_types_id' => $typeA->id,
+            'status' => 'Ativo',
+        ])->contentTags()->attach($tag->id);
+
+        Content::factory()->create([
+            'title' => 'Other Content',
+            'content_types_id' => $typeB->id,
+            'status' => 'Ativo',
+        ]);
+
+        $resSearch = $this->getJson('/api/conteudos?search=Searchable');
+        $resSearch->assertStatus(200)->assertJsonCount(1, 'data.data');
+
+        $resType = $this->getJson('/api/conteudos?content_type=Type A');
+        $resType->assertStatus(200)->assertJsonCount(1, 'data.data');
+
+        $resTag = $this->getJson('/api/conteudos?content_tag=mytag');
+        $resTag->assertStatus(200)->assertJsonCount(1, 'data.data');
+    }
+
+    public function test_content_belongs_to_content_type(): void
+    {
+        $type = ContentType::factory()->create();
+        $content = Content::factory()->create(['content_types_id' => $type->id]);
+
+        $this->assertEquals($type->id, $content->contentType->id);
+    }
+
+    public function test_content_has_many_tags(): void
+    {
+        $content = Content::factory()->withTags(2)->create();
+
+        $this->assertCount(2, $content->contentTags);
+        $this->assertInstanceOf(ContentTag::class, $content->contentTags->first());
+    }
+
+    public function test_content_belongs_to_creator(): void
+    {
+        $creator = \App\Models\SystemUser::factory()->create();
+        $content = Content::factory()->create(['criador' => $creator->id]);
+
+        $this->assertEquals($creator->id, $content->creator->id);
+        $this->assertInstanceOf(\App\Models\SystemUser::class, $content->creator);
+    }
+
+    public function test_content_belongs_to_last_editor(): void
+    {
+        $editor = \App\Models\SystemUser::factory()->create();
+        $content = Content::factory()->create(['ultimo_editor' => $editor->id]);
+
+        $this->assertEquals($editor->id, $content->lastEditor->id);
+        $this->assertInstanceOf(\App\Models\SystemUser::class, $content->lastEditor);
+    }
+
+    public function test_scope_ativo_returns_only_active_contents(): void
+    {
+        Content::factory()->create(['status' => 'Ativo']);
+        Content::factory()->create(['status' => 'Inativo']);
+
+        $activeContents = Content::Ativo()->get();
+
+        $this->assertCount(1, $activeContents);
+        $this->assertEquals('Ativo', $activeContents->first()->status);
+    }
+
+    public function test_deleting_content_removes_associated_tags(): void
+    {
+        $content = Content::factory()->withTags(2)->create();
+
+        $this->assertCount(2, $content->contentTags);
+
+        $content->delete();
+
+        $this->assertDatabaseMissing('content_tags', ['content_id' => $content->id]);
+    }
+
+    public function test_last_editor_is_updated_on_content_update(): void
+    {
+        $editor = \App\Models\SystemUser::factory()->create();
+        $content = Content::factory()->create();
+
+        $content->update(['ultimo_editor' => $editor->id]);
+
+        $this->assertEquals($editor->id, $content->ultimo_editor);
     }
 }
---
+++ tests/Unit/ContentModelTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class ContentModelTest extends TestCase
+{
+    public function test_content_belongs_to_content_type(): void
+    {
+        $contentTypeMock = $this->createMock(\App\Models\ContentType::class);
+        $content = $this->getMockBuilder(\App\Models\Content::class)
+            ->onlyMethods(['contentType'])
+            ->getMock();
+
+        $content->method('contentType')->willReturn($contentTypeMock);
+
+        $this->assertInstanceOf(\App\Models\ContentType::class, $content->contentType());
+    }
+
+    public function test_content_has_many_tags(): void
+    {
+        $contentTagMock = $this->createMock(\App\Models\ContentTag::class);
+        $content = $this->getMockBuilder(\App\Models\Content::class)
+            ->onlyMethods(['contentTags'])
+            ->getMock();
+
+        $content->method('contentTags')->willReturn(collect([$contentTagMock, $contentTagMock]));
+
+        $tags = $content->contentTags();
+        if ($tags instanceof \Illuminate\Database\Eloquent\Relations\Relation) {
+            $tags = $tags->get();
+        }
+
+        $this->assertCount(2, $tags);
+        $this->assertInstanceOf(\App\Models\ContentTag::class, $tags->first());
+    }
+
+    public function test_scope_ativo_returns_only_active_contents(): void
+    {
+        $queryMock = $this->getMockBuilder(\Illuminate\Database\Eloquent\Builder::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['where'])
+            ->getMock();
+
+        $queryMock->expects($this->once())
+            ->method('where')
+            ->with('status', 'Ativo')
+            ->willReturnSelf();
+
+        $content = $this->getMockBuilder(\App\Models\Content::class)
+            ->onlyMethods(['newQuery'])
+            ->getMock();
+
+        $content->method('newQuery')->willReturn($queryMock);
+
+        $this->assertSame($queryMock, $content->scopeAtivo($queryMock));
+    }
+
+    public function test_last_editor_is_updated_on_content_update(): void
+    {
+        $content = $this->getMockBuilder(\App\Models\Content::class)
+            ->onlyMethods(['update'])
+            ->getMock();
+
+        $content->expects($this->once())
+            ->method('update')
+            ->with(['ultimo_editor' => 1])
+            ->willReturn(true);
+
+        $this->assertTrue($content->update(['ultimo_editor' => 1]));
+    }
+
+    public function test_deleting_content_removes_associated_tags(): void
+    {
+        $relationMock = $this->getMockBuilder(\Illuminate\Database\Eloquent\Relations\BelongsToMany::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['detach'])
+            ->getMock();
+
+        $relationMock->expects($this->once())->method('detach');
+
+        $content = $this->getMockBuilder(\App\Models\Content::class)
+            ->onlyMethods(['contentTags', 'delete'])
+            ->getMock();
+
+        $content->method('contentTags')->willReturn($relationMock);
+
+        $content->expects($this->once())
+            ->method('delete')
+            ->willReturnCallback(function () use ($relationMock) {
+                $relationMock->detach();
+            });
+
+        $content->delete();
+    }
+}
