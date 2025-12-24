6ab085f4de394dac6f627080700cff92f008d62a
Ramalho7 <75459079+Ramalho7@users.noreply.github.com>
2025-12-24 00:37:23 +0000

    Apply automatic changes - pint

--- app/Mail/SuccessUpdateUser.php
+++ app/Mail/SuccessUpdateUser.php
@@ -4,7 +4,6 @@ namespace App\Mail;
 
 use DateTime;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
@@ -15,7 +14,9 @@ class SuccessUpdateUser extends Mailable
     use Queueable, SerializesModels;
 
     protected string $UserName;
+
     protected string $creatorName;
+
     protected DateTime $updatedAt;
 
     /**
